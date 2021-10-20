e195727a5661e31535ce31d14cbeb7c2d5d5a273
peibol888 <pabloalamovargas@gmail.com>
2021-10-20 10:30:25 +0000

    finished

--- src/js/component/light.jsx
+++ src/js/component/light.jsx
@@ -3,45 +3,10 @@ import PropTypes from "prop-types";
 
 //create your first component
 export const Light = props => {
-	const [glow, setGlow] = useState({
-		red: false,
-		orange: false,
-		green: false
-	});
-
-	const changeLight = () => {
-		let selectedColor = Object.entries(glow).find(
-			color => color[0] === props.color
-		)[0];
-
-		if (selectedColor === "red") {
-			glow["red"]
-				? setGlow({ red: false, orange: false, green: false })
-				: setGlow({ red: true, orange: false, green: false });
-		} else if (selectedColor === "orange") {
-			glow["orange"]
-				? setGlow({ red: false, orange: false, green: false })
-				: setGlow({ red: false, orange: true, green: false });
-		} else {
-			glow["green"]
-				? setGlow({ red: false, orange: false, green: false })
-				: setGlow({ red: false, orange: false, green: true });
-		}
-
-		console.log(glow);
-	};
-
-	return (
-		<div
-			className={
-				props.color +
-				"-light" +
-				(glow[props.color] ? " " + props.color + "-glow" : "")
-			}
-			onClick={changeLight}></div>
-	);
+	return <div className={props.classes} onClick={props.clickfunction}></div>;
 };
 
 Light.propTypes = {
-	color: PropTypes.string
+	classes: PropTypes.string,
+	clickfunction: PropTypes.func
 };
--- src/js/component/trafficlight.jsx
+++ src/js/component/trafficlight.jsx
@@ -1,12 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Light } from "./light.jsx";
 
 const TrafficLight = () => {
+	// Hooks
+	const [glow, setGlow] = useState({
+		red: false,
+		orange: false,
+		green: false
+	});
+
+	// Funcions
+	const setState = e => {
+		let newGlow = glow;
+		for (let key in newGlow) {
+			key === e.target.classList[0]
+				? (newGlow[key] = !newGlow[key])
+				: (newGlow[key] = false);
+		}
+		setGlow(prevGlow => {
+			return { ...prevGlow, ...newGlow };
+		});
+	};
+
+	// Render
 	return (
 		<div className="traffic-light">
-			<Light color="red" />
-			<Light color="orange" />
-			<Light color="green" />
+			<Light
+				classes={"red" + (glow.red ? " red-glow" : "")}
+				clickfunction={setState}
+			/>
+			<Light
+				classes={"orange" + (glow.orange ? " orange-glow" : "")}
+				clickfunction={setState}
+			/>
+			<Light
+				classes={"green" + (glow.green ? " green-glow" : "")}
+				clickfunction={setState}
+			/>
 		</div>
 	);
 };
